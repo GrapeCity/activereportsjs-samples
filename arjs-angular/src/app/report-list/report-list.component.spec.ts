9101e04ba6ff25121cc25f50360c16e4a7355bfe
Sergey Abakumov <Sabakumov@binck.nl>
2021-06-25 13:16:13 +0200

    version up

--- arjs-angular/src/app/report-list/report-list.component.spec.ts
+++ arjs-angular/src/app/report-list/report-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ReportListComponent } from './report-list.component';
 
@@ -6,7 +6,7 @@ describe('ReportListComponent', () => {
   let component: ReportListComponent;
   let fixture: ComponentFixture<ReportListComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ReportListComponent ]
     })
